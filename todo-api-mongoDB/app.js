a14b24f5ea96fc77c9f54312d2006b5f9c1bfb02
Chrisdanvic1 <chrisdanvic13@gmail.com>
2026-07-25 20:38:28 +0100

    commit

--- todo-api-mongoDB/app.js
+++ todo-api-mongoDB/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://todo-api-full.vercel.app/"],
+    origin: ["http://localhost:5173", "https://todo-api-full.vercel.app"],
     credentials: true,
   }),
 );
